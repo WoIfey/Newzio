d9eecee9eb3d104b86b4bd6b39cec5153389a320
Philip Huynh <wuflerz@outlook.com>
2025-05-09 00:40:49 +0300

    test

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,6 +1,6 @@
 export async function getUserNews(userId: string) {
     try {
-        const response = await fetch(`${process.env.URL}/news.json`)
+        const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/news.json`)
         const data = await response.json()
         const authorNews = data.filter((item: any) => item.user_id === userId)
         authorNews.sort((a: any, b: any) => b.id - a.id)
@@ -13,7 +13,7 @@ export async function getUserNews(userId: string) {
 
 export async function getPage(articleId: string) {
     try {
-        const response = await fetch(`${process.env.URL}/news.json`)
+        const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/news.json`)
         const data = await response.json()
         const article = data.filter((item: any) => item.id.toString() === articleId)
         return article
@@ -25,7 +25,7 @@ export async function getPage(articleId: string) {
 
 export async function getComment(commentId: string) {
     try {
-        const response = await fetch(`${process.env.URL}/comments.json`)
+        const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/comments.json`)
         const data = await response.json()
         const comment = data.filter((item: any) => item.id.toString() === commentId)
         return comment
